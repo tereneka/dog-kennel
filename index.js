2fefb8649bef1d044491bfa61a628fdb7561ef56
tereneka <teren-eka@yandex.ru>
2022-11-25 14:42:39 +0300

    fix: fix background for img-section

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".form");
+const formInputUserName = form.querySelector("#firstname");
+const popup = document.querySelector(".popup");
+const popupUserName = popup.querySelector(".popup__user-name");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  popupUserName.textContent = `${formInputUserName.value}`;
+  form.style.display = "none";
+  popup.classList.add("popup_opened");
+  form.reset();
+});
